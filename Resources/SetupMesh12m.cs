49170a9e0bb38b5baaaf65ab06625bc180621e94
andreharv <andreharv@yahoo.com>
2016-06-28 00:25:57 -0400

    Added some textures

---
+++ Resources/SetupMesh12m.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetroOverhaul.Resources
+{
+    class SetupMesh12m
+    {
+        public void SetupMesh(NetInfo info, NetInfoVersion version)
+        {
+
+        }
+    }
+}
