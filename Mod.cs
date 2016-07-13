27ee2bc8b9912c8bc782918d08f8e4017945c0bd
Egor Aralov <aei.omsk@gmail.com>
2016-07-14 00:16:19 +0600

    Change mod name

--- Mod.cs
+++ Mod.cs
@@ -8,7 +8,7 @@ namespace SingleTrainTrack
         public const string TRAIN_STATION_TRACK = "Train Station Track";
         public const string TRAIN_TRACK = "Train Track";
 
-        public string Name => "One-Way Tracks";
+        public string Name => "One-Way Train Tracks";
 
         public string Description => "Provides one-way train tracks";
     }
