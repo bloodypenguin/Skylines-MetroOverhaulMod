e96ffbe3ce2e487ceb64e8cee2f6baa9f869470c
earalov <earalov@thumbtack.net>
2016-11-14 16:38:47 +0600

    Prevent underground metro depots from being treated like underground metro stations

--- Extensions/BuildingInfoExtensions.cs
+++ Extensions/BuildingInfoExtensions.cs
@@ -6,12 +6,12 @@ namespace MetroOverhaul.Extensions
     {
         public static bool IsMetroDepot(this BuildingInfo info)
         {
-            return info?.m_class != null && info.m_buildingAI is DepotAI && info.m_class.m_subService == ItemClass.SubService.PublicTransportMetro;
+            return info?.m_class != null && info.m_buildingAI is DepotAI && info.m_class.m_service == ItemClass.Service.PublicTransport && info.m_class.m_subService == ItemClass.SubService.PublicTransportMetro;
         }
 
         public static bool IsUndergroundMetroStation(this BuildingInfo info)
         {
-            return IsMetroDepot(info) && info.m_paths != null && info.m_paths.Any(p => p?.m_netInfo != null && p.m_netInfo.IsUndergroundMetroStationTrack());
+            return IsMetroDepot(info) && info.m_buildingAI is TransportStationAI && info.m_paths != null && info.m_paths.Any(p => p?.m_netInfo != null && p.m_netInfo.IsUndergroundMetroStationTrack());
         }
     }
 }
