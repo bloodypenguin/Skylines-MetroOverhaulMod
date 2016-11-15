01a06dc088054f26cf9ecd2495fdc176a7acbd8c
earalov <earalov@thumbtack.net>
2016-11-15 22:17:16 +0600

    Add general options section

--- Options.cs
+++ Options.cs
@@ -9,6 +9,7 @@ namespace MetroOverhaul
     {
         private const string WIP = "WIP features";
         private const string STYLES = "Additional styles";
+        private const string GENERAL = "General settings";
         public Options()
         {
 #if DEBUG
@@ -21,21 +22,25 @@ namespace MetroOverhaul
             metroUi = true;
             replaceExistingNetworks = false;
         }
+        [Checkbox("Metro track customization UI (requires reloading from main menu)", GENERAL)]
+        public bool metroUi { set; get; }
+
+        [Checkbox("Improved PassengerTrainAI (Allows trains to return to depots)", GENERAL, nameof(PassengerTrainAIDetour), nameof(PassengerTrainAIDetour.ChangeDeployState))]
+        public bool improvedPassengerTrainAi { set; get; }
+
+        [Checkbox("Improved MetroTrainAI (Allows trains to properly spawn at surface)", GENERAL, nameof(MetroTrainAIDetour), nameof(MetroTrainAIDetour.ChangeDeployState))]
+        public bool improvedMetroTrainAi { set; get; }
+
         [Checkbox("Concrete tracks (no fence)", STYLES)]
         public bool concreteTracksNoBar { set; get; }
 
-        [Checkbox("Metro track customization UI (requires reloading from main menu)")]
-        public bool metroUi { set; get; }
-        [Checkbox("Improved PassengerTrainAI (Allows trains to return to depots)", null, nameof(PassengerTrainAIDetour), nameof(PassengerTrainAIDetour.ChangeDeployState))]
-        public bool improvedPassengerTrainAi { set; get; }
-        [Checkbox("Improved MetroTrainAI (Allows trains to properly spawn at surface)", null, nameof(MetroTrainAIDetour), nameof(MetroTrainAIDetour.ChangeDeployState))]
-        public bool improvedMetroTrainAi { set; get; }
         [Checkbox("Replace vanilla metro tracks with MOM tracks", WIP)]
         public bool replaceExistingNetworks { set; get; }
 
 #if DEBUG
         [Checkbox("Steel tracks", STYLES)]
         public bool steelTracks { set; get; }
+
         [Checkbox("Steel tracks (no fence)", STYLES)]
         public bool steelTracksNoBar { set; get; }
 #endif
