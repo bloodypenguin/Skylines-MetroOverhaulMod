f15cfbfd5c72011a6cb203c3facb5824cbe44004
calvin-fisher <calvin.fisher@gmail.com>
2017-06-01 07:37:54 -0500

    Added safety helper extensions

---
+++ Extensions/ArrayExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetroOverhaul.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static bool TryGetFromBuffer<T>(this Array16<T> array, uint index, out T result)
+        {
+            if (index >= array.m_buffer.Length)
+            {
+                result = default(T);
+                return false;
+            }
+
+            result = array.m_buffer[index];
+            return true;
+        }
+    }
+}
---
+++ Extensions/VehicleExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ColossalFramework;
+using UnityEngine;
+
+namespace MetroOverhaul.Extensions
+{
+    public static class VehicleExtensions
+    {
+        public static bool TryGetStartPosition(this Vehicle vehicleData, ushort vehicleId, out Vector3 startPosition)
+        {
+            if ((vehicleData.m_flags & Vehicle.Flags.Reversed) == (Vehicle.Flags)0)
+            {
+                startPosition = (Vector3)vehicleData.m_targetPos0;
+                return true;
+            }
+            else
+            {
+                var instance = Singleton<VehicleManager>.instance;
+                var lastVehicleIndex = (int)vehicleData.GetLastVehicle(vehicleId);
+
+                if (!instance.m_vehicles.TryGetFromBuffer((uint)lastVehicleIndex, out Vehicle lastVehicle))
+                {
+                    startPosition = Vector3.zero;
+                    return false;
+                }
+
+                startPosition = lastVehicle.m_targetPos0;
+                return true;
+            }
+        }
+    }
+}
