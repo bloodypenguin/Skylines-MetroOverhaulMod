2c34a9de89bad3632a3b41a5d0f80d60b62e5823
Egor Aralov <aei.omsk@gmail.com>
2017-06-10 12:27:40 +0200

    Make different mod name in patched version

--- Mod.cs
+++ Mod.cs
@@ -5,7 +5,13 @@ namespace MetroOverhaul
 {
     public class Mod : IUserMod
     {
-        public string Name => "Metro Overhaul";
+#if IS_PATCH
+        public const bool isPatch = true;
+#else
+        public const bool isPatch = false;
+#endif
+
+        public string Name => "Metro Overhaul" + (isPatch ? " [Patched]" : "");
         public string Description => "Brings metro depots, ground and elevated metro tracks";
 
         public void OnSettingsUI(UIHelperBase helper)
