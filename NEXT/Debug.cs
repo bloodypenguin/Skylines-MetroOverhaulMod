4eedf21009a6fb3f2a1fb89b1bd2a97db94e9cb7
andreharv <andreharv@yahoo.com>
2018-01-01 15:34:28 -0500

    Fixed for release version

--- NEXT/Debug.cs
+++ NEXT/Debug.cs
@@ -5,11 +5,12 @@ using System.Text;
 
 namespace Next
 {
-#if DEBUG
+
     public static class Debug
     {
         public static void Out(ColossalFramework.Plugins.PluginManager.MessageType messageType, bool useComma, params System.Object[] o)
         {
+#if DEBUG
             try
             {
                 string s = "";
@@ -24,42 +25,57 @@ namespace Next
             catch (Exception)
             {
             }
+#endif
         }
 
         public static void Log(params System.Object[] o)
         {
+#if DEBUG
             Message(o);
+#endif
         }
 
         public static void Message(params System.Object[] o)
         {
+#if DEBUG
             Message(true, o);
+#endif
         }
 
         public static void Message(bool useComma, params System.Object[] o)
         {
+#if DEBUG
             Out(ColossalFramework.Plugins.PluginManager.MessageType.Message, useComma, o);
+#endif
         }
 
         public static void Warning(params System.Object[] o)
         {
+#if DEBUG
             Warning(true, o);
+#endif
         }
 
         public static void Warning(bool useComma, params System.Object[] o)
         {
+#if DEBUG
             Out(ColossalFramework.Plugins.PluginManager.MessageType.Warning, useComma, o);
+#endif
         }
 
         public static void Error(params System.Object[] o)
         {
+#if DEBUG
             Error(true, o);
+#endif
         }
 
         public static void Error(bool useComma, params System.Object[] o)
         {
+#if DEBUG
             Out(ColossalFramework.Plugins.PluginManager.MessageType.Error, useComma, o);
+#endif
         }
     }
-#endif
+
 }
