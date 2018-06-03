5e0c60e090cc24df1f08fedc92fca00923798e40
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2018-06-03 22:39:32 +0200

    Add no depot option. Use typeof in OptionsFramework

--- Options.cs
+++ Options.cs
@@ -19,11 +19,14 @@ namespace MetroOverhaul
         }
         [Checkbox("Metro track customization UI (requires reloading from main menu)", GENERAL)]
         public bool metroUi { set; get; }
+        
+        [Checkbox("No depot mode (Trains will spawn at stations. Please bulldoze existing depots after enabling)", GENERAL)]
+        public bool depotsNotRequiredMode { set; get; }
 
-        [Checkbox("Improved PassengerTrainAI (Allows trains to return to depots)", GENERAL, nameof(PassengerTrainAIDetour), nameof(PassengerTrainAIDetour.ChangeDeployState))]
+        [Checkbox("Improved PassengerTrainAI (Allows trains to return to depots)", GENERAL, typeof(PassengerTrainAIDetour), nameof(PassengerTrainAIDetour.ChangeDeployState))]
         public bool improvedPassengerTrainAi { set; get; }
 
-        [Checkbox("Improved MetroTrainAI (Allows trains to properly spawn at surface)", GENERAL, nameof(MetroTrainAIDetour), nameof(MetroTrainAIDetour.ChangeDeployState))]
+        [Checkbox("Improved MetroTrainAI (Allows trains to properly spawn at surface)", GENERAL, typeof(MetroTrainAIDetour), nameof(MetroTrainAIDetour.ChangeDeployState))]
         public bool improvedMetroTrainAi { set; get; }
 
         [Checkbox("GHOST MODE (Load your MOM city with this ON and save before unsubscribing)", UNSUBPREP)]
