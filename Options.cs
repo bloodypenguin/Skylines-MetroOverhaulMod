9bdb6d92685a51117ebef1148f1033275db712ba
Bloody Penguin <bloodypenguin.skylines@gmail.com>
2018-06-04 00:06:46 +0200

    Temporarily commented out no depot mode

--- Options.cs
+++ Options.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Xml.Serialization;
 using MetroOverhaul.Detours;
 using MetroOverhaul.OptionsFramework.Attibutes;
 
@@ -19,8 +20,10 @@ namespace MetroOverhaul
         }
         [Checkbox("Metro track customization UI (requires reloading from main menu)", GENERAL)]
         public bool metroUi { set; get; }
-        
-        [Checkbox("No depot required mode (requires reloading from main menu)", GENERAL)]
+
+//TODO(bloodypengin): currently disabled because causes issues with trains not spawning at some types of stations       
+//        [Checkbox("No depot required mode (requires reloading from main menu)", GENERAL)]
+        [XmlIgnore]
         public bool depotsNotRequiredMode { set; get; }
 
         [Checkbox("Improved PassengerTrainAI (Allows trains to return to depots)", GENERAL, typeof(PassengerTrainAIDetour), nameof(PassengerTrainAIDetour.ChangeDeployState))]
